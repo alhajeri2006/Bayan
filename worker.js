1a82727558ad88182a4df03a2ee33d47e6a6e77c
alhajeri2006 <alhajeri2006@gmail.com>
2026-05-27 09:37:16 +0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
-const API_KEY = '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';
+const API_KEY = env.API_KEY;
 
 export default {
-  async fetch(request) {
+  async fetch(request, env) {
     if (request.method === 'OPTIONS') {
       return new Response(null, {
         headers: {
